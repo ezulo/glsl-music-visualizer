c4f868811d7908201dcfe743b2165ea8838afb17
Eduardo Zuloaga <ezuloaga0@gmail.com>
2026-01-12 15:49:06 -0500

    Fragment shader improvements: some nice functions to start with

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -6,32 +6,36 @@ uniform float u_time;
 uniform vec2 u_resolution;
 
 void main() {
+    vec2 uv_global, uv_local;
+    vec3 color, bg_color, final_color;
+    float dist_global, dist_local, wave_amplitude;
     // Normalize coordinates to [-1, 1] with aspect ratio correction
-    vec2 uv = (gl_FragCoord.xy * 2.0 - u_resolution) / min(u_resolution.x, u_resolution.y);
+    uv_global = (gl_FragCoord.xy * 2.0 - u_resolution) / min(u_resolution.x, u_resolution.y);
+    dist_global = length(uv_global);
 
-    // Circle parameters
-    float radius = 0.25;
-    float dist = length(uv);
+    uv_local = uv_global;
+    uv_local *= 2.0f;     // < 1.1x,              1.1y >
+    uv_local = fract(uv_local); // < fract(1.1x),       fract(1.1y) >
+    uv_local -= 0.5;      // < fract(1.1x) - 0.5, fract(1.1y) - 0.5 >
 
-    // Smooth circle edge
-    float circle = 1.0 - dist;
+    dist_local = length(uv_local);
+
+    // Wave amplitude
+    wave_amplitude = 0.5f + sin(dist_local * 8.0f + u_time * 2.0f) / 2.0f;
+    wave_amplitude = step(0.9f, wave_amplitude);
 
     // Animate color with time
-    //vec3 color = vec3(
-    //    0.5 + 0.5 * sin(u_time),
-    //    0.5 + 0.5 * sin(u_time + 2.094),
-    //    0.5 + 0.5 * sin(u_time + 4.188)
-    //);
-
-    // Color circle magenta
-    vec3 color = vec3(circle, 0, 0.0);
+    color = vec3(
+        0.5 * sin(dist_global + u_time),
+        0.5 * sin(dist_global + u_time + 2.094),
+        0.5 * sin(dist_global + u_time + 4.188)
+    );
 
     // Background color
-    vec3 bg_color = vec3(0.1, 0.1, 0.15);
+    bg_color = vec3(0.1, 0.1, 0.15);
 
     // Mix circle and background
-    //vec3 final_color = mix(bg_color, color, circle);
-    vec3 final_color = color;
+    final_color = mix(bg_color, color, wave_amplitude);
 
     FragColor = vec4(final_color, 1.0);
 }
